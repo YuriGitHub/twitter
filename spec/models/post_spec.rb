25aa6f820821748188f5801d0f4ecfb1cd5c2bcc
Yuri <yura89peregudov@gmail.com>
2016-08-10 14:19:52 +0300

    added test for post validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe Post, type: :model do
         should have_many :images
         should have_many :comments
     end
+# validates :text, length: { in: 10...140}
+# 	validates :text, :user_id, presence: true
+    describe 'Testing validateion' do 
+    		it 'checking validation text length' do
+    			should validate_length_of(:text).is_at_most(139).is_at_least(10)
+    		end
+
+    		it 'Testing presence of fields' do
+    			should validate_presence_of(:text)
+    			should validate_presence_of(:user_id)
+    		end
+    end
 end
