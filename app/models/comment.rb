26d461e82b02f1bb41c6d21a43bef647e322d85a
Yuri <yura89peregudov@gmail.com>
2016-08-10 15:45:00 +0300

    fixed association name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
 
 	validates :text, length: {maximum: 50}
 	validates :text, :user_id, :post_id, presence: true
-	has_many :reports, as: :repotable
+	has_many :reports, as: :reportable
 end
