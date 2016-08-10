46f5e3a75cb5b96b40cb73a16ef084a6ca76d67d
Yuri <yura89peregudov@gmail.com>
2016-08-10 11:38:38 +0300

    fix conflict

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 RSpec.describe Comment, type: :model do
-<<<<<<< HEAD
+
     it 'checks relations' do
         should belong_to :post
     end
-=======
-  it 'test for post ' do
- should belong_to(:post)
-end
->>>>>>> d42db668f20f11074786cc0a599c317414bc0b58
+
+
 end
