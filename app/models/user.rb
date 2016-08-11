b7abb02959a6ffe3a04200aa5caca87a0573a2af
Dmitry <Dmitry.Smetyukhov@gmail.com>
2016-08-11 15:27:23 +0300

    registration omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,12 +27,20 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+
+#binding.pry
     user.email = auth.info.email
     user.login = auth.info.email
     user.password = Devise.friendly_token[0,20]
     user.name = auth.info.name   # assuming the user model has a name
-    user.first_name = auth.info.first_name
-    user.last_name = auth.info.last_name
+    if auth.info.first_name
+       user.first_name = auth.info.first_name       
+       user.last_name = auth.info.last_name
+    else
+      user.first_name = auth.info.name.split(' ')[0]
+      user.last_name = auth.info.name.split(' ')[1]
+    end
+
    # user.image = auth.info.image # assuming the user model has an image
   end
 end
@@ -41,6 +49,7 @@ def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
+       # user.first_name = data["first_name"] if user.first_name.blank?
       end
     end
   end
