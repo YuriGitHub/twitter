4d233e3c50fbcf3dad48bce01012d1abd4f9c0be
bliareg <bliareg3@gmail.com>
2016-08-12 14:28:22 +0300

    added root route and home routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
  
+
 devise_for :users, :controllers => {:registrations => "users/registrations"}
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-
+  
+  root :to => 'home#index'
+  get 'home/followers'=>'home#followers'
+  get 'home/toggle_follow'=>'home#toggle_follow'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'search' => 'search#search'
