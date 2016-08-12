de9c17ba78240e8e1bf1d8a53711c574dbcf26a3
bliareg <bliareg3@gmail.com>
2016-08-12 18:18:29 +0300

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
  
 
-devise_for :users, :controllers => {:registrations => "users/registrations"}
 
-  devise_for :users, :controllers => { :registrations => "users/registrations" ,:omniauth_callbacks => "users/omniauth_callbacks", :passwords =>'registrations' }
+  devise_for :users, :controllers => { :registrations => "users/registrations" ,:omniauth_callbacks => "users/omniauth_callbacks"}
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
