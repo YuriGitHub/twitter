2e7a18fe2a234d2d56d520a8c6ee1342d0804cf7
bliareg <bliareg3@gmail.com>
2016-08-12 14:29:05 +0300

    updated controller for returning jsons

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,22 @@ class SearchController < ApplicationController
   
   # Search user
   def search
-    @search = Sunspot.search( User ) do
+    search = Sunspot.search( User ) do
      fulltext params[:query]
     end
-    @users = @search.results
+    results = search.results 
+        respond_to do |format|
+        format.json{
+            results.each do |u|
+                if current_user.followers.find_by_id(u.id) != nil
+                    u.is_follower = 'following'
+                else
+                    u.is_follower = 'not_following'
+                end
+            end
+            render json: search.results
+        }
+    end
     
-    render 'search/index'
   end
 end
