c41eb2f629c5009d2d6548474412706ffc3531d8
bliareg <bliareg3@gmail.com>
2016-08-15 23:27:19 +0300

    added root path and home paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
+
   devise_for :users, :controllers => { :registrations => "users/registrations" ,:omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   ActiveAdmin.routes(self)
-
+  
+  root :to => 'home#index'
+  get 'home/ifollow'=>'home#ifollow'
+  get 'home/toggle_follow'=>'home#toggle_follow'
 
   get '/report_to_unlock/', to: 'send_reports#show_report'
   post '/report_to_unlock/:id/', to: 'send_reports#add_report'
@@ -14,5 +18,7 @@ Rails.application.routes.draw do
   post '/admin/unlock_user/:id', to: 'admin/users#unlock_user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'search' => 'search#search'
-  post 'user/add_report/:id_reportable', to: 'send_reports#add_report_to_user'
+  post 'user/add_report/:id_reportable', to: 'send_reports#add_report_to_user', as: 'send_report'
+ 
+  #resources :users, only: [:show]
 end
