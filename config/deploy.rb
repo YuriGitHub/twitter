286074b621da6437bcdf7d29089321c458cb9199
Yuri <yura89peregudov@gmail.com>
2016-08-16 23:44:17 +0300

    refactor

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,13 @@
 # Change these
 server '37.139.4.141',  roles: [:web, :app, :db], primary: true
 
+set :branch, 'template'
 set :repo_url,        'git@github.com:YuriGitHub/twitter.git'
 set :application,     'twitter'
 set :user,            'twitter_deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :branch, 'template'
+
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
