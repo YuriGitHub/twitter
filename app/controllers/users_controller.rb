5b49ba11a2ff6065a7cff4b05be83cdeb5c9507a
bliareg <bliareg3@gmail.com>
2016-08-16 16:43:48 +0300

    added follow button in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
 
 	def show
 	    @user = User.find(params[:id])
+            @is_following = false
+            if current_user != nil
+                @is_following = true if current_user.ifollow.find_by_id(@user.id) != nil
+            end
 	end 
 end
