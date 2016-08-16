e7ff3d2e748d06468e3ded177bc83ddb9df5c52f
Yuri <yura89peregudov@gmail.com>
2016-08-16 23:37:02 +0300

    refactor

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application,     'twitter'
 set :user,            'twitter_deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-
+set :branch, 'template'
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
