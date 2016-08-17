9a1dc71c580dee46210691a1fb26240229274f86
SBlazhko <blaghco@ukr.net>
2016-08-17 14:48:29 +0300

    post ajax first version

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -8,9 +8,9 @@ $('#new_post_but').click(function(){
 $("#new_post").on("ajax:success", function(e, data, status, xhr) {
 	    var post = JSON.parse(xhr.responseText);		
       $(".post_index").append('<div class="panel panel-default">' +
-                             // '<div class="panel-heading">' +
-                             // '<h5>Created : ' + post.created_at + '</h5>' +
                              '<div class="panel-body">' +
+                             '<h5>Created : ' + post.created_at + '</h5>' +
+                             '<h5>--------------------------------------------</h5>'  +
                              '<h5>' + post.text + '</h5>' + '</div>' ); 
        $(".new_post_form").toggle();
     }).on("ajax:error", function(e, xhr, status, error) {
