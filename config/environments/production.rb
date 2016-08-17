72353e8d8f937f98bf25198ac2898bdaa199b722
Yuri <yura89peregudov@gmail.com>
2016-08-17 18:33:11 +0300

    added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,21 +57,6 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "twitter_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-    config.app_domain = 'http://37.139.4.141'
-
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.default_url_options = { host: config.app_domain }
-    config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: '587',
-      enable_starttls_auto: true,
-      user_name: 'test.dima.sm',
-      password: '7777777s',
-      authentication: :plain,
-      domain: 'www.somedomain.com'
-    }
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -98,6 +83,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-
 end
