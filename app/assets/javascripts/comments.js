62f2f073c5d1886128df40317e1f3d134695615e
Dmitry <Dmitry.Smetyukhov@gmail.com>
2016-08-19 12:15:42 +0300

    comments styles

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){	
 
+	var newCommentForms = $(".new_comment");
+	$.each(newCommentForms, function(key, obj){
+		$(obj).on("keypress", function(e){
+			if(e.which == 13 ){
+				$(this).submit();
+			}
+		});
+	});
+
 	//comments to posts
 	var arrShowCommentsAreaLinks = $('.show-comments-area-link');
 	$.each(arrShowCommentsAreaLinks, function(key, obj){
