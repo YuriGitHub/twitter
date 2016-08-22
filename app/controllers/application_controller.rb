95d6421a4392d0dc4fab9520d6e3005a7245ae5c
mrshaman <shaman12353@gmail.com>
2016-08-22 09:28:40 +0300

    internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,20 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
-    if user_signed_in? and params[:locale]
+    if user_signed_in?
+      if params[:locale]
         current_user.locale = params[:locale]
         current_user.save
-     # I18n.locale = current_user.try(:locale) || I18n.default_locale
+      else
+        if cookies[:locale] and current_user.locale.nil?
+          current_user.locale = cookies[:locale]
+          current_user.save
+        end
+      end
     else
-      session[:locale] = params[:locale] if params[:locale]
-     # I18n.locale = session[:locale] || I18n.default_locale
+      cookies[:locale] = params[:locale] if params[:locale]
     end
-    I18n.locale = current_user.try(:locale) || session[:locale] || I18n.default_locale
+    I18n.locale = current_user.try(:locale) || cookies[:locale] || I18n.default_locale
   end
 
   protected
