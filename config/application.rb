a51522a0339ee32d75f2e12aaee23550e5845c28
programisty4 <yura89peregudov@gmail.com>
2016-08-30 15:19:32 +0300

    fixed

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Twitter
   class Application < Rails::Application
-    config.action_cable.mount_path = '/chats'
+    # config.action_cable.mount_path = '/chats'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
