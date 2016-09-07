8321a27ec4e9249bbed654acaa5dac07d492d83d
Yuri <yura89peregudov@gmail.com>
2016-09-07 08:06:16 +0000

    fixed

--- app/channels/chats_channel.rb
+++ app/channels/chats_channel.rb
@@ -13,7 +13,7 @@ class ChatsChannel < ApplicationCable::Channel
  end
  def send_message(data)
    unless(current_user.premission?(params[:id]))
-   Message.create(text_message: data[:text],chat_room_id: params[:id],user_id: current_user.id)
+   Message.create(text_message: data['text'],chat_room_id: params[:id],user_id: current_user.id)
    puts data['text']
    puts self.current_user.login
    ActionCable.server.broadcast "chat#{params['id']}", message: data['text'], image: current_user.avatar(:thumb), login: current_user.login
