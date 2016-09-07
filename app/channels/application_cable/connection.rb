688df55588432559171f7042cc2aa60c783b2f48
programisty4 <yura89peregudov@gmail.com>
2016-09-07 11:08:09 +0300

    saved

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -6,17 +6,17 @@ module ApplicationCable
              self.current_user = find_verified_user
              logger.add_tags 'ActionCable', current_user
            end
+
+
+
            protected
              def find_verified_user
-
                if verified_user = User.find_by(ws_token: request.params[:token])
                  verified_user
-
                else
-              
                  reject_unauthorized_connection
 
                end
              end
-      end
-    end
+  end
+end
