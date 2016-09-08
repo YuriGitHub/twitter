61bea3d5c4d19d38bd92d71eee4e51bb6fdf41cd
bliareg <bliareg3@gmail.com>
2016-09-08 14:17:23 +0300

    added post collapsing and changed search style

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    
+
     belongs_to :user
     has_many :likes
     has_many :attachments
@@ -8,11 +8,20 @@ class Post < ApplicationRecord
 
     validates :text, length: { in: 5...500}
     validates :text, :user_id, presence: true
-    validates :text,length: {in: 5...50}
+    validates :header,length: {in: 5...100}
 
     attr_accessor :videos
     attr_accessor :images
     attr_accessor :audio
+
+    def short_content(type,length)
+        text;
+        length = 20 if length == nil or length == 0
+        type = "text" if type == nil or type == ""
+        text = self.header if type == "header"
+        text = self.text if type == "text"
+        text[0..length]
+    end
 
     def set_attachments
         self.images = self.attachments.image
@@ -20,9 +29,9 @@ class Post < ApplicationRecord
         self.audio = self.attachments.audio
     end
 
-        #search posts by text
-        searchable do
-            text :text
-            text :header
-        end
+    #search posts by text
+    searchable do
+        text :text
+        text :header
+    end
 end
